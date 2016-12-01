1791107af7e207dbc0fd4b0baf7df80bdbf17ab1
Alexandra <alexandraserralta@yahoo.com>
2016-12-01 17:17:08 -0500

    fixed

--- src/main.ts
+++ src/main.ts
@@ -46,6 +46,22 @@ interface ICommunityMatch extends IStateTransition {
   playerIdToProposal: IProposals; 
 }
 
+interface ICommunityChat {
+  messageList:any;
+  messageForm:any;
+  messageInput:any;
+  submitButton:any;
+  submitImageButton:any;
+  imageForm:any;
+  mediaCapture:any; 
+  userPic:any;
+  userName:any;
+  signInButton:any;
+  signOutButton:any;
+  signInSnackbar:any;
+}
+
+
 export let $rootScope: angular.IScope;
 export let $location: angular.ILocationService;
 export let $timeout: angular.ITimeoutService;
@@ -60,22 +76,24 @@ declare var firebase: any;
 export module main {
   // Feel free to change to your own Firebase URL :)
   // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDvbUblHfA5eJe5sK1Xy-xC_tfV4y4PgQE",
-    authDomain: "signalling-d073b.firebaseapp.com",
-    databaseURL: "https://signalling-d073b.firebaseio.com",
-    storageBucket: "signalling-d073b.appspot.com",
-    messagingSenderId: "308144322392"
+var config = {
+    apiKey: "AIzaSyCUw1qMmgowubEhDQ6AzkK2zBjORf591do",
+    authDomain: "chess-chat-f38c0.firebaseapp.com",
+    databaseURL: "https://chess-chat-f38c0.firebaseio.com",
+    storageBucket: "chess-chat-f38c0.appspot.com",
+    messagingSenderId: "47081620857"
   };
   firebase.initializeApp(config);
   export let matches: ICommunityMatch[] = [];
   // Saving as json because firebase has restriction on keys (and we use "data: any").
   // Example error: Firebase.set failed: First argument  contains an invalid key (playerId0.5446834512026781) in property 'matches.0.playerIdToProposal'.  Keys must be non-empty strings and can't contain ".", "#", "$", "/", "[", or "]"
   // Another weird thing: For some reason firebase stores "{}" as null (for playerIdToProposal).
+  
   let matchesRef = firebase.database().ref("matchesJson");
   matchesRef.on('value', function(snapshot: any) {
     $timeout(()=> {
       let matchesJson = snapshot.val();
+      console.log(matchesJson);
       if (!matchesJson) {
         matches = createCommunityMatches(); 
         storeMatches();
@@ -86,6 +104,7 @@ export module main {
     });
   });
 
+  export let communityChat: ICommunityChat = getChatData();
   export let myPlayerInfo: MyPlayerInfo = getMyPlayerInfo();
   log.alwaysLog("myPlayerInfo=", myPlayerInfo);
 
@@ -104,6 +123,30 @@ export module main {
     localStorage.setItem("myPlayerInfoJson", angular.toJson(myPlayerInfo));
     return myPlayerInfo;
   }
+
+
+
+  function getChatData() {
+    communityChat = {
+      messageList: document.getElementById('messages'),
+      messageForm: document.getElementById('message-form'),
+      messageInput: document.getElementById('message'),
+      submitButton: document.getElementById('submit'),
+      submitImageButton: document.getElementById('submitImage'),
+      imageForm: document.getElementById('image-form'),
+      mediaCapture: document.getElementById('mediaCapture'),
+      userPic: document.getElementById('user-pic'),
+      userName: document.getElementById('user-name'),
+      signInButton: document.getElementById('sign-in'),
+      signOutButton: document.getElementById('sign-out'),
+      signInSnackbar: document.getElementById('must-signin-snackbar'),
+    };
+
+
+
+    return communityChat;
+  }
+
 
   function storeMatches() {
     matchesRef.set(angular.toJson(matches));
@@ -194,149 +237,6 @@ export module main {
     lastCommunityUI = communityUI;
     messageSender.sendToGame({communityUI: communityUI});
   }
-
-
-  function FriendlyChat() {
-  this.checkSetup();
-
-  // Shortcuts to DOM Elements.
-  this.messageList = document.getElementById('messages');
-  this.messageForm = document.getElementById('message-form');
-  this.messageInput = document.getElementById('message');
-  this.submitButton = document.getElementById('submit');
-  this.submitImageButton = document.getElementById('submitImage');
-  this.imageForm = document.getElementById('image-form');
-  this.mediaCapture = document.getElementById('mediaCapture');
-  this.userPic = document.getElementById('user-pic');
-  this.userName = document.getElementById('user-name');
-  this.signInButton = document.getElementById('sign-in');
-  this.signOutButton = document.getElementById('sign-out');
-  this.signInSnackbar = document.getElementById('must-signin-snackbar');
-
-  // Saves message on form submit.
-  this.messageForm.addEventListener('submit', this.saveMessage.bind(this));
-  this.signOutButton.addEventListener('click', this.signOut.bind(this));
-  this.signInButton.addEventListener('click', this.signIn.bind(this));
-
-  // Toggle for the button.
-  var buttonTogglingHandler = this.toggleButton.bind(this);
-  this.messageInput.addEventListener('keyup', buttonTogglingHandler);
-  this.messageInput.addEventListener('change', buttonTogglingHandler);
-
-  // Events for image upload.
-  this.submitImageButton.addEventListener('click', function() {
-    this.mediaCapture.click();
-  }.bind(this));
-  this.mediaCapture.addEventListener('change', this.saveImageMessage.bind(this));
-
-  this.initFirebase();
-}
-
-// Sets up shortcuts to Firebase features and initiate firebase auth.
-FriendlyChat.prototype.initFirebase = function() {
-  // TODO(DEVELOPER): Initialize Firebase.
-};
-
-// Loads chat messages history and listens for upcoming ones.
-FriendlyChat.prototype.loadMessages = function() {
-  // TODO(DEVELOPER): Load and listens for new messages.
-};
-
-// Saves a new message on the Firebase DB.
-FriendlyChat.prototype.saveMessage = function(e:any) {
-  e.preventDefault();
-  // Check that the user entered a message and is signed in.
-  if (this.messageInput.value && this.checkSignedInWithMessage()) {
-
-    // TODO(DEVELOPER): push new message to Firebase.
-
-  }
-};
-
-// Sets the URL of the given img element with the URL of the image stored in Firebase Storage.
-FriendlyChat.prototype.setImageUrl = function(imageUri:any, imgElement:any) {
-  imgElement.src = imageUri;
-
-  // TODO(DEVELOPER): If image is on Firebase Storage, fetch image URL and set img element's src.
-};
-
-// Saves a new message containing an image URI in Firebase.
-// This first saves the image in Firebase storage.
-FriendlyChat.prototype.saveImageMessage = function(event:any) {
-  var file = event.target.files[0];
-
-  // Clear the selection in the file picker input.
-  this.imageForm.reset();
-
-  // Check if the file is an image.
-  if (!file.type.match('image.*')) {
-    var data = {
-      message: 'You can only share images',
-      timeout: 2000
-    };
-    this.signInSnackbar.MaterialSnackbar.showSnackbar(data);
-    return;
-  }
-  // Check if the user is signed-in
-  if (this.checkSignedInWithMessage()) {
-
-    // TODO(DEVELOPER): Upload image to Firebase storage and add message.
-
-  }
-};
-
-// Signs-in Friendly Chat.
-FriendlyChat.prototype.signIn = function() {
-  // TODO(DEVELOPER): Sign in Firebase with credential from the Google user.
-};
-
-// Signs-out of Friendly Chat.
-FriendlyChat.prototype.signOut = function() {
-  // TODO(DEVELOPER): Sign out of Firebase.
-};
-
-// Triggers when the auth state change for instance when the user signs-in or signs-out.
-FriendlyChat.prototype.onAuthStateChanged = function(user:any) {
-  if (user) { // User is signed in!
-    // Get profile pic and user's name from the Firebase user object.
-    var profilePicUrl:any = null;   // TODO(DEVELOPER): Get profile pic.
-    var userName:any = null;        // TODO(DEVELOPER): Get user's name.
-
-    // Set the user's profile pic and name.
-    this.userPic.style.backgroundImage = 'url(' + profilePicUrl + ')';
-    this.userName.textContent = userName;
-
-    // Show user's profile and sign-out button.
-    this.userName.removeAttribute('hidden');
-    this.userPic.removeAttribute('hidden');
-    this.signOutButton.removeAttribute('hidden');
-
-    // Hide sign-in button.
-    this.signInButton.setAttribute('hidden', 'true');
-
-    // We load currently existing chant messages.
-    this.loadMessages();
-  } else { // User is signed out!
-    // Hide user's profile and sign-out button.
-    this.userName.setAttribute('hidden', 'true');
-    this.userPic.setAttribute('hidden', 'true');
-    this.signOutButton.setAttribute('hidden', 'true');
-
-    // Show sign-in button.
-    this.signInButton.removeAttribute('hidden');
-  }
-};
-
-
-// Enables or disables the submit button depending on the values of the input
-// fields.
-FriendlyChat.prototype.toggleButton = function() {
-  if (this.messageInput.value) {
-    this.submitButton.removeAttribute('disabled');
-  } else {
-    this.submitButton.setAttribute('disabled', 'true');
-  }
-};
 
 
 
--- ts_output_readonly_do_NOT_change_manually/src/main.js
+++ ts_output_readonly_do_NOT_change_manually/src/main.js
@@ -5,11 +5,11 @@ var gamingPlatform;
         // Feel free to change to your own Firebase URL :)
         // Initialize Firebase
         var config = {
-            apiKey: "AIzaSyDvbUblHfA5eJe5sK1Xy-xC_tfV4y4PgQE",
-            authDomain: "signalling-d073b.firebaseapp.com",
-            databaseURL: "https://signalling-d073b.firebaseio.com",
-            storageBucket: "signalling-d073b.appspot.com",
-            messagingSenderId: "308144322392"
+            apiKey: "AIzaSyCUw1qMmgowubEhDQ6AzkK2zBjORf591do",
+            authDomain: "chess-chat-f38c0.firebaseapp.com",
+            databaseURL: "https://chess-chat-f38c0.firebaseio.com",
+            storageBucket: "chess-chat-f38c0.appspot.com",
+            messagingSenderId: "47081620857"
         };
         firebase.initializeApp(config);
         main.matches = [];
@@ -20,6 +20,7 @@ var gamingPlatform;
         matchesRef.on('value', function (snapshot) {
             gamingPlatform.$timeout(function () {
                 var matchesJson = snapshot.val();
+                console.log(matchesJson);
                 if (!matchesJson) {
                     main.matches = createCommunityMatches();
                     storeMatches();
@@ -31,6 +32,7 @@ var gamingPlatform;
                 }
             });
         });
+        main.communityChat = getChatData();
         main.myPlayerInfo = getMyPlayerInfo();
         gamingPlatform.log.alwaysLog("myPlayerInfo=", main.myPlayerInfo);
         function getMyPlayerInfo() {
@@ -47,6 +49,23 @@ var gamingPlatform;
             };
             localStorage.setItem("myPlayerInfoJson", angular.toJson(main.myPlayerInfo));
             return main.myPlayerInfo;
+        }
+        function getChatData() {
+            main.communityChat = {
+                messageList: document.getElementById('messages'),
+                messageForm: document.getElementById('message-form'),
+                messageInput: document.getElementById('message'),
+                submitButton: document.getElementById('submit'),
+                submitImageButton: document.getElementById('submitImage'),
+                imageForm: document.getElementById('image-form'),
+                mediaCapture: document.getElementById('mediaCapture'),
+                userPic: document.getElementById('user-pic'),
+                userName: document.getElementById('user-name'),
+                signInButton: document.getElementById('sign-in'),
+                signOutButton: document.getElementById('sign-out'),
+                signInSnackbar: document.getElementById('must-signin-snackbar'),
+            };
+            return main.communityChat;
         }
         function storeMatches() {
             matchesRef.set(angular.toJson(main.matches));
@@ -134,120 +153,6 @@ var gamingPlatform;
             lastCommunityUI = communityUI;
             gamingPlatform.messageSender.sendToGame({ communityUI: communityUI });
         }
-        function FriendlyChat() {
-            this.checkSetup();
-            // Shortcuts to DOM Elements.
-            this.messageList = document.getElementById('messages');
-            this.messageForm = document.getElementById('message-form');
-            this.messageInput = document.getElementById('message');
-            this.submitButton = document.getElementById('submit');
-            this.submitImageButton = document.getElementById('submitImage');
-            this.imageForm = document.getElementById('image-form');
-            this.mediaCapture = document.getElementById('mediaCapture');
-            this.userPic = document.getElementById('user-pic');
-            this.userName = document.getElementById('user-name');
-            this.signInButton = document.getElementById('sign-in');
-            this.signOutButton = document.getElementById('sign-out');
-            this.signInSnackbar = document.getElementById('must-signin-snackbar');
-            // Saves message on form submit.
-            this.messageForm.addEventListener('submit', this.saveMessage.bind(this));
-            this.signOutButton.addEventListener('click', this.signOut.bind(this));
-            this.signInButton.addEventListener('click', this.signIn.bind(this));
-            // Toggle for the button.
-            var buttonTogglingHandler = this.toggleButton.bind(this);
-            this.messageInput.addEventListener('keyup', buttonTogglingHandler);
-            this.messageInput.addEventListener('change', buttonTogglingHandler);
-            // Events for image upload.
-            this.submitImageButton.addEventListener('click', function () {
-                this.mediaCapture.click();
-            }.bind(this));
-            this.mediaCapture.addEventListener('change', this.saveImageMessage.bind(this));
-            this.initFirebase();
-        }
-        // Sets up shortcuts to Firebase features and initiate firebase auth.
-        FriendlyChat.prototype.initFirebase = function () {
-            // TODO(DEVELOPER): Initialize Firebase.
-        };
-        // Loads chat messages history and listens for upcoming ones.
-        FriendlyChat.prototype.loadMessages = function () {
-            // TODO(DEVELOPER): Load and listens for new messages.
-        };
-        // Saves a new message on the Firebase DB.
-        FriendlyChat.prototype.saveMessage = function (e) {
-            e.preventDefault();
-            // Check that the user entered a message and is signed in.
-            if (this.messageInput.value && this.checkSignedInWithMessage()) {
-            }
-        };
-        // Sets the URL of the given img element with the URL of the image stored in Firebase Storage.
-        FriendlyChat.prototype.setImageUrl = function (imageUri, imgElement) {
-            imgElement.src = imageUri;
-            // TODO(DEVELOPER): If image is on Firebase Storage, fetch image URL and set img element's src.
-        };
-        // Saves a new message containing an image URI in Firebase.
-        // This first saves the image in Firebase storage.
-        FriendlyChat.prototype.saveImageMessage = function (event) {
-            var file = event.target.files[0];
-            // Clear the selection in the file picker input.
-            this.imageForm.reset();
-            // Check if the file is an image.
-            if (!file.type.match('image.*')) {
-                var data = {
-                    message: 'You can only share images',
-                    timeout: 2000
-                };
-                this.signInSnackbar.MaterialSnackbar.showSnackbar(data);
-                return;
-            }
-            // Check if the user is signed-in
-            if (this.checkSignedInWithMessage()) {
-            }
-        };
-        // Signs-in Friendly Chat.
-        FriendlyChat.prototype.signIn = function () {
-            // TODO(DEVELOPER): Sign in Firebase with credential from the Google user.
-        };
-        // Signs-out of Friendly Chat.
-        FriendlyChat.prototype.signOut = function () {
-            // TODO(DEVELOPER): Sign out of Firebase.
-        };
-        // Triggers when the auth state change for instance when the user signs-in or signs-out.
-        FriendlyChat.prototype.onAuthStateChanged = function (user) {
-            if (user) {
-                // Get profile pic and user's name from the Firebase user object.
-                var profilePicUrl = null; // TODO(DEVELOPER): Get profile pic.
-                var userName = null; // TODO(DEVELOPER): Get user's name.
-                // Set the user's profile pic and name.
-                this.userPic.style.backgroundImage = 'url(' + profilePicUrl + ')';
-                this.userName.textContent = userName;
-                // Show user's profile and sign-out button.
-                this.userName.removeAttribute('hidden');
-                this.userPic.removeAttribute('hidden');
-                this.signOutButton.removeAttribute('hidden');
-                // Hide sign-in button.
-                this.signInButton.setAttribute('hidden', 'true');
-                // We load currently existing chant messages.
-                this.loadMessages();
-            }
-            else {
-                // Hide user's profile and sign-out button.
-                this.userName.setAttribute('hidden', 'true');
-                this.userPic.setAttribute('hidden', 'true');
-                this.signOutButton.setAttribute('hidden', 'true');
-                // Show sign-in button.
-                this.signInButton.removeAttribute('hidden');
-            }
-        };
-        // Enables or disables the submit button depending on the values of the input
-        // fields.
-        FriendlyChat.prototype.toggleButton = function () {
-            if (this.messageInput.value) {
-                this.submitButton.removeAttribute('disabled');
-            }
-            else {
-                this.submitButton.setAttribute('disabled', 'true');
-            }
-        };
         window.addEventListener("message", function (event) {
             var game_iframe = window.document.getElementById("game_iframe");
             if (!game_iframe || game_iframe.contentWindow !== event.source) {
